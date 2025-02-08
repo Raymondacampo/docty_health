08c4cce8ecf949b6d92539219e29a2e7092bc0db
Raymondacampo <raymondacamposandoval@gmail.com>
2025-02-08 15:18:36 -0400

    deploy test 7

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ data }) {
       <div>
           <h1>Next.js + Django API</h1>
           <h2>Test prueba 3</h2>
-          <h4>Aver si se actualizo!!!!!!dfdf</h4>
+          <h4>Aver si se actualizo!!!!!!dfdfd</h4>
           <p>{data.message}</p>
       </div>
   );
