c8c003bf0dd51f9bee197554eabc64f54c8f13a1
Raymondacampo <raymondacamposandoval@gmail.com>
2025-02-08 13:38:18 -0400

    testing git hook for auto updating 7

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ data }) {
       <div>
           <h1>Next.js + Django API</h1>
           <h2>Test prueba 3</h2>
-          <h4>Aver si se actualizo!!!!!!!!!!!!!2222brgher</h4>
+          <h4>Aver si se actualizo!!!!!!!!!!!!!2222brghertrrthrt</h4>
           <p>{data.message}</p>
       </div>
   );
