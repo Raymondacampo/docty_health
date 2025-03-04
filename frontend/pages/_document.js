ccb5e39026835a2cffd3acbe3c27f75147bf6bfa
Raymondacampo <raymondacamposandoval@gmail.com>
2025-03-04 16:06:38 -0400

    added inter font to document file

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
