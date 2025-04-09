02264e1336373279f36b435c3598366c7a1743af
Jacques <jacquesacampo@gmail.com>
2025-04-09 19:14:56 -0400

    loc implementation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,10 +27,10 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-SECURE_SSL_REDIRECT = False
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ['localhost', 'juanpabloduarte.com']
 
