5a7116b84b451504aa02581660a0a11f14625d70
Raymondacampo <raymondacamposandoval@gmail.com>
2025-04-14 23:17:57 -0400

    overflow hidden page layout

--- frontend/components/PageLayout.js
+++ frontend/components/PageLayout.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 export default function PageLayout({ children }) {
   return (
-    <div className="w-full flex flex-col min-h-screen">
+    <div className="w-full flex flex-col overflow-hidden min-h-screen">
       <Head />
       <main className="flex-grow font-['Inter']">{children}</main>
       <Footer/>
