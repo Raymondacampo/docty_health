edc1d03448149538550afc3d43e36742bfd4e969
Karibu <karibu@gmail.com>
2025-05-30 02:45:00 -0400

    DoctyHealth 1.1

--- frontend/components/PageLayout.js
+++ frontend/components/PageLayout.js
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 export default function PageLayout({ children }) {
   return (
     <div className="w-full flex flex-col overflow-hidden min-h-screen bg-gray-200/30">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <Head />
       <main className="flex-grow font-['Inter']">{children}</main>
       <Footer/>
