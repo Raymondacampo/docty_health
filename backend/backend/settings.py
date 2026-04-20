4d32c6367be0de51b909a7d19a4d411f49192796
Raymondacampo <raymondacamposandoval@gmail.com>
2026-04-20 16:31:02 -0400

    render added to allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ CSRF_COOKIE_SECURE = get_bool_env('PROD_SSL', True)
 SECURE_SSL_REDIRECT = get_bool_env('PROD_SSL', True)
 
 # ALLOWED_HOSTS = ['localhost', 'juanpabloduarte.com', '127.0.0.1', '192.168.1.x']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'docty-health-1.onrender.com']
 APPEND_SLASH = True
 
 # Application definition
